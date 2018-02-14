702ebcd4ccca8004e3bd01d99bc0e8f1076191e4
Oriol Gual <oriol.gual@gmail.com>
2018-02-14 12:56:20 +0100

    Publish feature settings change

--- decidim-assemblies/app/controllers/decidim/assemblies/admin/features_controller.rb
+++ decidim-assemblies/app/controllers/decidim/assemblies/admin/features_controller.rb
@@ -8,6 +8,16 @@ module Decidim
       #
       class FeaturesController < Decidim::Admin::FeaturesController
         include Concerns::AssemblyAdmin
+
+        def handle_feature_settings_change(previous_settings, current_settings)
+          ActiveSupport::Notifications.publish(
+            "decidim.features.settings_change",
+            participatory_space_id: @feature.participatory_space.id,
+            feature_id: @feature.id,
+            current_feature_settings: previous_settings,
+            previous_feature_settings: current_settings
+          )
+        end
       end
     end
   end
--- decidim-participatory_processes/app/controllers/decidim/participatory_processes/admin/features_controller.rb
+++ decidim-participatory_processes/app/controllers/decidim/participatory_processes/admin/features_controller.rb
@@ -8,6 +8,9 @@ module Decidim
       #
       class FeaturesController < Decidim::Admin::FeaturesController
         include Concerns::ParticipatoryProcessAdmin
+
+        # We don't need to publish anything here since it's done when a step is activated.
+        def handle_feature_settings_change(_previous_settings, _current_settings); end
       end
     end
   end
